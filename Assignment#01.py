1169bd3225c4ba49ef1b2d5ea30eddb5e630bd16
Asmeer Farooqi <your-email@example.com>
2025-03-17 11:59:30 -0700

    Assignment#01 Uploaded

---
+++ Assignment#01.py
@@ -0,0 +1,73 @@
+'''Name = "\tAsmeer-ul-haq \n"
+Fathername ="\ttanveer-ul-haq \n"
+DOB = "\t20-sep-2003 \n"
+
+print(Name,Fathername,DOB)'''
+
+
+'''Q#02'''
+'''Name = "Asmeer-Ul-Haq "
+Fathername ="tanveer-ul-haq \n"
+DOB = "20-sep-2003 "
+Age = 21
+Weight = 75.60 
+Profession = "cloud data engineer \n" 
+skills = ["python","java","machine learning \n"]
+
+print("=====MY BIO=======")
+print("Name:" ,Name)
+print("Fathername:", Fathername)
+print("DOB:" ,DOB)
+print("Age:" , Age)
+print("Weight:" ,Weight)
+print("Profession:",Profession)
+print("Skills:",skills)'''
+
+'''q#03'''
+''''a = 45
+b =23
+print("Multiplation:", a*b)
+print("Addition:" ,a+b)
+print("Subtraction:", a-b)
+print("Division:" ,a/b)
+print("Modulus:", a%b)
+print("floor division:", a//b)
+print("Exponentiation:" ,a**b)'''
+
+'''a = int(input("Enter a number a:"))
+b = int(input("Enter a number b:"))
+print("Floor Division:",a//b)'''
+
+
+'''Q#04'''
+'''Maths = 65
+English = 78
+Islamiat = 90
+TotalMarks = 300
+obtainedmarks = Maths+English+Islamiat
+print ("percentage of all subjects:" ,obtainedmarks/300*100)'''
+
+'''Q#05'''
+'''Addnumber = int(input("your last number:"))
+lastdigit = Addnumber % 10
+print("Last digit:",lastdigit)'''
+
+
+'''Q#06'''
+quantity = int(input("Enter purchased quantity"))
+cost  =  quantity *100
+
+if cost > 1000:
+    discount = cost * 10/100
+    cost -= discount
+    print("Total cost:" , cost)
+    print("Discount is eligible")
+else:
+    print("sorry! discount not eligible")
+
+
+
+
+
+
+
